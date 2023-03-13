e1886ae9e0ab9a0aebf68b1096ddf9d99418a8ae
Nusaibarh <umarnusaiba965@gmail.com>
2023-03-14 00:19:58 +0100

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-
-const arg = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let step = 0; step < 3; step++) {
-  console.log(arg[step]);
+const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < strings.length; i++) {
+  console.log(strings[i]);
 }
