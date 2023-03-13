e4e33c9f94ff77a05ca815c6c24b14bc28fd2407
Nusaibarh <umarnusaiba965@gmail.com>
2023-03-14 00:20:01 +0100

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
+const { argv } = require('process');
 
-const arg = 'C is fun';
-const arrg = process.argv;
-const step = parseInt(arrg[2]);
-if (isNaN(step)) {
+if (isNaN(argv[2])) {
   console.log('Missing number of occurrences');
 } else {
-  for (let start = 0; start < step; start++) {
-    console.log(arg);
+  const number = parseInt(argv[2]);
+  for (let i = 0; ((i >= 0) && (i < number)); i++) {
+    console.log('C is fun');
   }
 }
