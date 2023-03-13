1dd06fe77228a845f7cf227b0c29ac0fb9dd371a
Nusaibarh <umarnusaiba965@gmail.com>
2023-03-14 00:19:34 +0100

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,17 +1,11 @@
 #!/usr/bin/node
+const { argv } = require('process');
 
-const arg = process.argv;
-const len = arg.length;
-function secondlarge (l, array) {
-  if (l < 4) {
-    return (0);
-  }
-  array.splice(0, 2);
-  const num = Math.max(...array);
-  const strint = num.toString();
-  let large = array.indexOf(strint);
-  array.splice(large, 1);
-  large = Math.max(...array);
-  return (large);
+if (argv.length <= 3) {
+  console.log(0);
+} else {
+  let newArgv = argv.slice(2, argv.length);
+  newArgv = newArgv.map((value) => parseInt(value));
+  newArgv.sort((a, b) => b - a);
+  console.log(newArgv[1]);
 }
-console.log(secondlarge(len, arg));
