079c9b82b140e250de1d5a35d3f8eb487b33d14c
AbdurrahmanIdr <abdurrahmaneedrees@gmail.com>
2023-03-23 13:24:23 +0100

    7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
-"""Start link class to table in database
 """
-import sys
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
+This script lists all State objects
+from the database `hbtn_0e_6_usa`.
+"""
+
+from sys import argv
+from model_state import State, Base
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    url = 'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
-                                                      sys.argv[2], sys.argv[3])
-    engine = create_engine(url, pool_pre_ping=True)
+    """
+    Access to the database and get the states
+    from the database.
+    """
 
-    connection = engine.connect()
-    Base.metadata.create_all(engine)
-    session = sessionmaker(bind=engine)()
-    results = session.query(State).all()
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
 
-    for states in results:
-        print("{}: {}".format(states.id, states.name))
+    engine = create_engine(db_url)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    for instance in session.query(State).order_by(State.id):
+        print('{0}: {1}'.format(instance.id, instance.name))
