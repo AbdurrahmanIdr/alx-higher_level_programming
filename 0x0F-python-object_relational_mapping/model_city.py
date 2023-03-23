883b2e02ced3e13d63288e29a3bd859451cc0e4f
AbdurrahmanIdr <abdurrahmaneedrees@gmail.com>
2023-03-23 13:15:57 +0100

    model_city.py

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python3
 """
-This script defines a City class
-to work with MySQLAlchemy ORM.
+python script that contains the class
+definition of a State and an instance
+Base = declarative_base:
 """
-
-from model_state import Base, State
 from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
 
 
 class City(Base):
-    """City class
+    """A City clas that links to MySql table ciy"""
 
-    Attributes:
-        __tablename__ (str): The table name of the class
-        id (int): The id of the class
-        name (str): The name of the class
-        state_id (int): The state the city belongs to
+    __tablename__ = "cities"
 
-    """
-    __tablename__ = 'cities'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    id = Column(Integer, autoincrement=True, unique=True,
+                nullable=False, primary_key=True)
+    name = Column(String(length=128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
