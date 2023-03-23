f97d6a61faa564feb31c5a457531b23ef3af6083
AbdurrahmanIdr <abdurrahmaneedrees@gmail.com>
2023-03-23 13:23:53 +0100

    10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
-"""Start link class to table in database
 """
-import sys
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
+This script prints the first State object
+from the database `hbtn_0e_6_usa`.
+"""
+
+from sys import argv
+from model_state import State, Base
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    url = 'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
-                                                      sys.argv[2], sys.argv[3])
-    engine = create_engine(url, pool_pre_ping=True)
+    """
+    Access to the database and get a state
+    from the database.
+    """
 
-    connection = engine.connect()
-    Base.metadata.create_all(engine)
-    session = sessionmaker(bind=engine)()
-    results = session.query(State).all()
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
 
-    count = 1
-    for states in results:
-        if states.name == sys.argv[4]:
-            print(states.id)
-            count = 0
-            break
-    if count:
+    engine = create_engine(db_url)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    state = session.query(State).filter(State.name == argv[4]).first()
+    if state is not None:
+        print('{0}'.format(state.id))
+    else:
         print("Not found")
