78ca1517f295d6a9e6cb196c008243efa16c3fd3
AbdurrahmanIdr <abdurrahmaneedrees@gmail.com>
2023-03-23 13:15:34 +0100

    4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,37 +1,20 @@
 #!/usr/bin/python3
 """
-This script lists all cities from
-the database `hbtn_0e_4_usa`.
+python script that lists all states from the database hbtn_0e_0_usa
 """
-
 import MySQLdb
-from sys import argv
+import sys
 
-if __name__ == '__main__':
-    """
-    Access to the database and get the cities
-    from the database.
-    """
-
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
-
-    with db.cursor() as cur:
-        cur.execute("""
-            SELECT
-                cities.id, cities.name, states.name
-            FROM
-                cities
-            JOIN
-                states
-            ON
-                cities.state_id = states.id
-            ORDER BY
-                cities.id ASC
-        """)
-
-        rows = cur.fetchall()
-
-    if rows is not None:
-        for row in rows:
-            print(row)
+if __name__ == "__main__":
+    if len(sys.argv) >= 4:
+        db_connection = MySQLdb.connect(
+                host="localhost", port=3306,
+                user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]
+                )
+        c = db_connection.cursor()
+        c.execute("SELECT c.id, c.name, s.name FROM cities as c \
+INNER JOIN states as s ON c.state_id = s.id ORDER  BY c.id;")
+        results = c.fetchall()
+        for result in results:
+            print(result)
+        db_connection.close
