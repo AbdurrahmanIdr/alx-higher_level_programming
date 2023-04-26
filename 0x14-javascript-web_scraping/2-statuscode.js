d11cb2205e30e54bb07e6b22277c220e167b953f
AbdurrahmanIdr <abdurrahmaneedrees@gmail.com>
2023-04-27 00:05:22 +0100

    2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
+// script that display the status code of a GET request
+// The status code must be printed like this: code: <status code>
 
 const request = require('request');
-request(process.argv[2], function (error, response) {
-  if (error) {
-    console.error(error);
-  }
-  console.log('code:', response && response.statusCode);
+const route = process.argv[2];
+
+request(route, (err, res, body) => {
+  if (!err) console.log('code:', res.statusCode);
 });
