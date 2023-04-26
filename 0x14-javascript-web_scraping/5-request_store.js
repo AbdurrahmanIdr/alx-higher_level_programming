2dea6fcb772488a9e9cc627f6d4ac6763b92e248
AbdurrahmanIdr <abdurrahmaneedrees@gmail.com>
2023-04-27 00:05:35 +0100

    5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,15 +1,19 @@
 #!/usr/bin/node
-// gets the body and write to a file
-
+// script that gets the contents of a webpage
+// and stores it in a file
 const request = require('request');
 const fs = require('fs');
-request(process.argv[2], function (error, response, body) {
-  if (error) {
-    console.error(error);
+const route = process.argv[2];
+const fileName = process.argv[3];
+
+request(route, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(fileName, body, 'utf8', function (err) {
+      if (err) {
+        console.log(err);
+      }
+    });
   }
-  fs.writeFile(process.argv[3], body, 'utf-8', (err) => {
-    if (err) {
-      console.error(err);
-    }
-  });
 });
