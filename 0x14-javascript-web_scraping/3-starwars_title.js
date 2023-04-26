fc3b78dcb030ad73d9b4dea26ba2337093c78a27
AbdurrahmanIdr <abdurrahmaneedrees@gmail.com>
2023-04-27 00:05:26 +0100

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
+// script that prints the title of a Star Wars movie where
+// the episode number matches a given integer
 
 const request = require('request');
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response) {
-  if (error) {
-    console.error(error);
-  }
-  console.log(JSON.parse(response.body).title);
+const movieId = process.argv[2];
+const route = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+
+request(route, (err, res, body) => {
+  if (err) throw err;
+  // Display the retrieved content
+  console.log(JSON.parse(body).title);
 });
