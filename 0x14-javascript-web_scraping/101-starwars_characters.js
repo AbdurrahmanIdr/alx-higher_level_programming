1aa65773d850c7259e2b3b8818922fc44bceffdd
AbdurrahmanIdr <abdurrahmaneedrees@gmail.com>
2023-04-27 00:05:13 +0100

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,29 +1,27 @@
 #!/usr/bin/node
+// script that prints all characters of a Star Wars movie
+
 const request = require('request');
-const url = 'http://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request.get(url, function (err, response) {
-  if (!err) {
-    const charlist = JSON.parse(response.body).characters;
-    console.log(charlist);
-    let i = 0;
-    while (i < charlist.length) {
-      now(charlist[i], i);
-    }
+const endPoint = 'http://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request.get(endPoint, function (err, response, body) {
+  if (err) {
+    throw err;
+  } else if (response.statusCode === 200) {
+    const characters = JSON.parse(body).characters;
+    const l = [];
+    characters.forEach(character => {
+      l.push(new Promise((resolve, reject) => {
+        request.get(character, function (err, response, body) {
+          if (err) {
+            reject(err);
+          } else if (response.statusCode === 200) {
+            resolve(JSON.parse(body).name);
+          }
+        });
+      }));
+    });
+    Promise.all(l).then(names => {
+      names.forEach(name => console.log(name));
+    });
   }
 });
-
-async function now (urlfun, count) {
-  request.get(urlfun, function (error, response) {
-    if (!error) {
-      const people = JSON.parse(response.body).name;
-      console.log(people);
-    }
-  });
-  await sleep(5000);
-  console.log('wait over');
-  count++;
-}
-
-async function sleep (ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
