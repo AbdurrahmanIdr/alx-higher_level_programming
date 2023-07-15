50ec465cdf22a71ccee31a74df33d5525525fb08
AbdurrahmanIdr <abdurrahmaneedrees@gmail.com>
2023-07-15 17:01:46 +0100

    infinite add edit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    sum = 0
-    for a in range(1, len(argv)):
-        sum += int(argv[a])
-    print(sum)
+
+    args = argv[1:]
+    result = sum(int(arg) for arg in args)
+
+    print(result)
